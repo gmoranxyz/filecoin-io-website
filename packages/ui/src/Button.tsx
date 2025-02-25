bfcc577ada898d620999e17308d08e269e0de44f
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-02-25 13:19:12 +0100

    [FF][FFDW][UXIT-2230][UXIT-2243] Adapt Button component for shared UI library (#1142)
    * Move Button to shared UI library
    
    * Include button styles
    
    * Adapt imports
    
    * CR

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,78 @@
+import { BaseLink, type BaseLinkProps } from '@filecoin-foundation/ui/BaseLink'
+import {
+  Icon as IconComponent,
+  type IconProps,
+} from '@filecoin-foundation/ui/Icon'
+import { isExternalLink } from '@filecoin-foundation/utils/linkUtils'
+import { ArrowUpRight } from '@phosphor-icons/react/dist/ssr'
+import { clsx } from 'clsx'
+
+const variantClasses = {
+  primary: 'button-primary',
+  ghost: 'button-ghost',
+}
+
+type ButtonProps = {
+  children: React.ReactNode
+  variant?: keyof typeof variantClasses
+  icon?: IconProps['component']
+  href?: BaseLinkProps['href']
+  baseDomain: string
+} & React.ComponentPropsWithoutRef<'button'>
+
+type ButtonInnerProps = Pick<ButtonProps, 'children' | 'icon'> & {
+  isExternalLink?: boolean
+}
+
+function ButtonInner({
+  icon: Icon,
+  children,
+  isExternalLink,
+}: ButtonInnerProps) {
+  return (
+    <>
+      {!isExternalLink && Icon && <IconComponent component={Icon} />}
+      {children}
+      {isExternalLink && <IconComponent component={ArrowUpRight} />}
+    </>
+  )
+}
+
+export function Button({
+  variant = 'primary',
+  className,
+  icon,
+  children,
+  disabled,
+  href,
+  baseDomain,
+  ...rest
+}: ButtonProps) {
+  className = clsx(
+    'button-base focus:brand-outline inline-flex items-center justify-center gap-2 px-6 py-3 font-semibold transition hover:no-underline sm:px-9',
+    variantClasses[variant],
+    {
+      'button-disabled disabled:pointer-events-none': disabled,
+    },
+    className,
+  )
+
+  if (typeof href === 'undefined') {
+    return (
+      <button className={className} disabled={disabled} {...rest}>
+        <ButtonInner icon={icon}>{children}</ButtonInner>
+      </button>
+    )
+  }
+
+  return (
+    <BaseLink className={className} href={href} baseDomain={baseDomain}>
+      <ButtonInner
+        isExternalLink={isExternalLink(href, baseDomain)}
+        icon={icon}
+      >
+        {children}
+      </ButtonInner>
+    </BaseLink>
+  )
+}
