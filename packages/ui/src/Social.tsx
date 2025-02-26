e83e45516d9cd92837a80bc71ad6907bf13c0971
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-02-26 15:05:37 +0100

    [FF][FFDW][UXIT-2257] Adapt Social component for shared UI Library (#1147)
    * Move Social component to shared UI library
    
    * Extend CTASection with children prop
    
    * Move types
    
    * Adapt imports
    
    * Scout CR - Use ExternalLink
    
    * Create socialConfig
    
    * Adapt social data
    
    * Use Social component
    
    * CR
    
    * CR

---
+++ packages/ui/src/Social.tsx
@@ -0,0 +1,47 @@
+import { Icon, type IconProps } from '@filecoin-foundation/ui/Icon'
+import type { TouchTarget } from '@filecoin-foundation/utils/types/touchTargetType'
+import { clsx } from 'clsx'
+
+type linkWithIcon = {
+  label: string
+  href: string
+  icon: IconProps['component']
+}
+
+type SocialProps = {
+  linksWithIcons: linkWithIcon[]
+}
+const TOUCH_TARGET: TouchTarget = {
+  touchAreaPadding: 'p-2',
+  touchAreaOffset: '-m-2 sm:mx-0',
+}
+
+export function Social({ linksWithIcons }: SocialProps) {
+  return (
+    <ul
+      className={clsx(
+        'flex flex-wrap items-center justify-between gap-4',
+        TOUCH_TARGET.touchAreaOffset,
+      )}
+    >
+      {linksWithIcons.map(({ label, href, icon }) => {
+        return (
+          <li key={label} className="inline-flex">
+            <a
+              aria-label={`Go to ${label}`}
+              href={href}
+              rel="noopener noreferrer"
+              title={`Go to ${label}`}
+              className={clsx(
+                'hover:text-brand-400 focus:brand-outline',
+                TOUCH_TARGET.touchAreaPadding,
+              )}
+            >
+              <Icon component={icon} size={32} weight="light" />
+            </a>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
