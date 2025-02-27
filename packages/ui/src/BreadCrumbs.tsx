11d91b54231f9a558f346b264318e05d5faaa17b
Charly MARTIN <charly@hey.com>
2025-02-27 10:29:10 +0200

    Move Breadcrumbs to shared components [skip percy] (#1148)
    * Move capitalize utility to shared utils library
    
    * Refactor BreadCrumbs label truncation with new utility function
    
    * Refactor BreadCrumbs component for shared UI library
    
    * Add breadcrumbs CSS classes to global styles
    
    * fixup! Refactor BreadCrumbs component for shared UI library
    
    * Remove dependencies to peerDependencies
    
    * Simplify BreadCrumbs component with InternalTextLink and pathname hook
    
    * Move BreadCrumbsLayout to shared UI library and update imports
    
    * fixup! Simplify BreadCrumbs component with InternalTextLink and pathname hook
    
    * Implement Breadcrumbs on FFDW
    
    * Simplify BreadCrumbs component by using internal constants
    
    * CR
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>

---
+++ packages/ui/src/BreadCrumbs.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { clsx } from 'clsx'
+import { usePathname } from 'next/navigation'
+import { Icon } from '@filecoin-foundation/ui/Icon'
+import { InternalTextLink } from '@filecoin-foundation/ui/TextLink'
+import { capitalize, truncate } from '@filecoin-foundation/utils/stringUtils'
+import { CaretRight } from '@phosphor-icons/react/dist/ssr'
+import type { Route } from 'next'
+
+const HOME_PATH = '/'
+const HOME_LABEL = 'Home'
+
+export function BreadCrumbs() {
+  const pathname = usePathname()
+  const pathNames = ['/'].concat(pathname.split('/').filter(Boolean))
+
+  return (
+    <nav aria-label="breadcrumbs">
+      <ol className="inline-flex items-center gap-2.5">
+        {pathNames.map((path, index) => {
+          const isRoot = index === 0
+
+          const href = isRoot
+            ? HOME_PATH
+            : '/' + pathNames.slice(1, index + 1).join('/')
+
+          const isActive = pathname === href
+          const label = isRoot ? capitalize(HOME_LABEL) : formatLabel(path)
+
+          return (
+            <li key={href} className="inline-flex items-center gap-2.5">
+              {!isRoot && (
+                <Icon
+                  component={CaretRight}
+                  size={20}
+                  color="subtle"
+                  weight="bold"
+                />
+              )}
+              <InternalTextLink
+                href={href as Route}
+                className={clsx(
+                  'focus:brand-outline hover:underline',
+                  !isActive && 'breadcrumbs-inactive',
+                  isActive && 'breadcrumbs-active',
+                )}
+              >
+                {label}
+              </InternalTextLink>
+            </li>
+          )
+        })}
+      </ol>
+    </nav>
+  )
+}
+
+function formatLabel(path: string) {
+  const MAX_LABEL_LENGTH = 20
+
+  const label = path.split('-').map(capitalize).join(' ')
+  return truncate(label, MAX_LABEL_LENGTH)
+}
