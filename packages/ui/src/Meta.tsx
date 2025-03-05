db3ceb6a41c57fa7308db88767c85887d8072dc5
Charly MARTIN <charly@hey.com>
2025-03-05 11:21:06 +0000

    [FF][FFDW] Make Card shareable [skip percy] (#1178)
    * Make Card shareable
    
    * Style: Update Meta component and add meta CSS class

---
+++ packages/ui/src/Meta.tsx
@@ -0,0 +1,31 @@
+import { Fragment } from 'react'
+
+import { clsx } from 'clsx'
+
+type MetaProps = {
+  metaData: MetaDataType
+}
+
+export type MetaDataType = Array<string | null | undefined>
+
+export function Meta({ metaData }: MetaProps) {
+  if (metaData.length === 0) {
+    return null
+  }
+
+  return (
+    <div className="meta flex flex-wrap gap-x-2 gap-y-1">
+      {metaData.filter(Boolean).map((data, index) => {
+        const isNotLastItem = index < metaData.length - 1
+        const isNotFirstItem = index > 0
+
+        return (
+          <Fragment key={index}>
+            <span className={clsx({ capitalize: isNotFirstItem })}>{data}</span>
+            {isNotLastItem && <span> | </span>}
+          </Fragment>
+        )
+      })}
+    </div>
+  )
+}
