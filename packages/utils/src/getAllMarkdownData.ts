80d201268fc15202b01cc960be69ba45e57c3c33
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-03-06 14:18:21 +0100

    [FF][FFDW] Move shared getMarkdown functions [skip percy] (#1193)
    * Move getMarkdownData to shared Utils package
    
    * Adapt imports
    
    * Update dependencies
    
    * Move getAllMarkdownData to shared Utils package
    
    * Adapt imports

---
+++ packages/utils/src/getAllMarkdownData.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import path from 'path'
+
+import { extractSlugFromFilename } from '@filecoin-foundation/utils/fileUtils'
+import { getMarkdownData } from '@filecoin-foundation/utils/getMarkdownData'
+import glob from 'fast-glob'
+import { ZodObject, type ZodRawShape } from 'zod'
+
+
+export type GetAllMarkdownDataArgs<T extends ZodRawShape> = {
+  directoryPath: string
+  zodSchema: ZodObject<T>
+}
+
+export async function getAllMarkdownData<T extends ZodRawShape>({
+  directoryPath,
+  zodSchema,
+}: GetAllMarkdownDataArgs<T>) {
+  try {
+    const directory = path.join(process.cwd(), directoryPath)
+
+    const filenames = await glob('**/*.md', {
+      cwd: directory,
+    })
+
+    return Promise.all(
+      filenames.map((filename) => {
+        const slug = extractSlugFromFilename(filename)
+        return getMarkdownData({ slug, directoryPath, zodSchema })
+      }),
+    )
+  } catch (error) {
+    console.error('Error retrieving all data:', error)
+    throw error
+  }
+}
