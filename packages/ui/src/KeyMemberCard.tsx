73d55cd4745c7f9d42da07156ee95467cd341b73
Charly MARTIN <charly@hey.com>
2025-03-07 11:48:42 +0000

    FFDW About Page (#1195)
    * Add KeyMemberCard component to UI library
    
    * fixup! Add KeyMemberCard component to UI library
    
    * Add BoardMemberGrid and KeyMemberCard to about page
    
    * fixup! Add KeyMemberCard component to UI library
    
    * fixup! Add BoardMemberGrid and KeyMemberCard to about page
    
    * Implement about page
    
    * Refactor PageSection components
    
    * Update apps/ffdweb-site/src/app/_components/CTALink.tsx
    
    Co-authored-by: Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
    
    * Update apps/ffdweb-site/src/app/_components/CTALink.tsx
    
    Co-authored-by: Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
    
    * CR
    
    ---------
    
    Co-authored-by: Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
    Co-authored-by: Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
    Co-authored-by: Mirha Masala <mirha@hey.com>

---
+++ packages/ui/src/KeyMemberCard.tsx
@@ -0,0 +1,88 @@
+import Image from 'next/image'
+
+import { Heading } from '@filecoin-foundation/ui/Heading'
+import { Icon } from '@filecoin-foundation/ui/Icon'
+import type {
+  StaticImageProps,
+  ImageProps,
+} from '@filecoin-foundation/utils/types/imageType'
+import { LinkedinLogo } from '@phosphor-icons/react/dist/ssr'
+import { clsx } from 'clsx'
+
+type KeyMemberCardProps = {
+  name: string
+  title: string
+  company?: string
+  linkedin: string
+  image: StaticImageProps['data'] | ImageProps['src']
+  as?: React.ElementType
+}
+
+export function KeyMemberCard({
+  name,
+  title,
+  company,
+  linkedin,
+  image,
+  as: Tag = 'li',
+}: KeyMemberCardProps) {
+  const fullTitle = [title, company].filter(Boolean).join(', ')
+
+  return (
+    <Tag className="key-member-card relative flex">
+      <KeyMemberImage image={image} name={name} />
+
+      <div className="m-3 grow">
+        <Heading tag="h3" variant="lg">
+          {name}
+        </Heading>
+
+        <p className="key-member-card-title mb-10 mt-1">{fullTitle}</p>
+
+        <a
+          aria-label={`Visit ${name}'s LinkedIn profile.`}
+          href={linkedin}
+          rel="noopener noreferrer"
+          className="key-member-card-link focus:brand-outline absolute inset-0"
+        >
+          <span className="absolute bottom-4 left-36 inline-flex items-center gap-2">
+            <Icon component={LinkedinLogo} />
+            LinkedIn
+          </span>
+        </a>
+      </div>
+    </Tag>
+  )
+}
+
+function KeyMemberImage({
+  image,
+  name,
+}: Pick<KeyMemberCardProps, 'image' | 'name'>) {
+  const commonProps = {
+    src: image,
+    alt: `Photo of ${name}`,
+    quality: 100,
+    sizes: '150px',
+    className: 'key-member-card-image object-cover',
+  }
+
+  const containerClass = 'aspect-3/4 w-32 shrink-0'
+  const isStaticImage = typeof image === 'object'
+
+  if (isStaticImage) {
+    return (
+      <Image
+        {...commonProps}
+        className={clsx(commonProps.className, containerClass)}
+        alt={commonProps.alt}
+      />
+    )
+  }
+
+  return (
+    <div className={clsx('relative', containerClass)}>
+      <Image fill {...commonProps} alt={commonProps.alt} />
+    </div>
+  )
+}
