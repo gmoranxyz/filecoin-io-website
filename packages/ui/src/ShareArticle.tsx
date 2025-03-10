011e575b48991ed8f2a35c6b6dd42dd714d39f45
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-03-10 15:25:23 +0100

    [FFDW][UXIT-2304] Share Article component [skip percy] (#1187)
    * feat: shared share article component
    
    * refactor: add type to function
    
    * fix: use classname
    
    * CR
    
    * CR
    
    * fix: arrow classname and add shareArticle to digest
    
    * Revert "fix: arrow classname and add shareArticle to digest"
    
    This reverts commit 89889b5037b4fe5f7ccc0bfcbb9996a7cafb63a9.
    
    * fix: missing dependency
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>

---
+++ packages/ui/src/ShareArticle.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { CopyToClipboard } from '@filecoin-foundation/ui/CopyToClipboard'
+import { Icon } from '@filecoin-foundation/ui/Icon'
+import { Tooltip } from '@filecoin-foundation/ui/Tooltip/Tooltip'
+import type { TouchTarget } from '@filecoin-foundation/utils/types/touchTargetType'
+import { clsx } from 'clsx'
+
+import { generateShareArticleLinks } from '@filecoin-foundation/utils/generateShareArticleLinks'
+
+type ShareArticleProps = {
+  articleTitle: string
+  path: string
+  baseUrl: string
+  sectionTitle?: string
+}
+
+const TOUCH_TARGET: TouchTarget = {
+  touchAreaPadding: 'p-2',
+  touchAreaOffset: '-m-2',
+}
+
+export function ShareArticle({
+  articleTitle,
+  path,
+  sectionTitle = 'Share Article',
+  baseUrl,
+}: ShareArticleProps) {
+  const articleUrl = `${baseUrl}${path}`
+
+  const socialLinksWithIcons = generateShareArticleLinks({
+    URL: articleUrl,
+    title: articleTitle,
+    BASE_URL: baseUrl,
+  })
+
+  return (
+    <div className="space-y-3">
+      <p className="share-article-title font-bold capitalize">{sectionTitle}</p>
+      <ul
+        className={clsx(
+          'flex flex-wrap items-center gap-6',
+          TOUCH_TARGET.touchAreaOffset,
+        )}
+      >
+        <li>
+          <CopyToClipboard
+            text={articleUrl}
+            notificationTitle="Link copied to clipboard!"
+          />
+        </li>
+        {socialLinksWithIcons.map(({ label, href, icon }) => (
+          <li key={label} className="inline-flex">
+            <Tooltip description={`Share on ${label}`} side="bottom">
+              <a
+                aria-label={`Share on ${label}`}
+                href={href}
+                rel="noopener noreferrer"
+                className={clsx(
+                  'share-article-tooltip-link focus:brand-outline',
+                  TOUCH_TARGET.touchAreaPadding,
+                )}
+              >
+                <Icon component={icon} size={32} weight="light" />
+              </a>
+            </Tooltip>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
