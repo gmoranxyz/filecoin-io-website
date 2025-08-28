5c1d958a4189b7f6614e6b6c120fbda0a3ba1c61
Barbara Peric <50910606+barbaraperic@users.noreply.github.com>
2025-08-28 12:22:23 +0200

    [FILECOIN][UXIT-3195] Refactor Cypress task and util functions [skip percy] (#1725)
    * feat(cypress): add end-to-end tests and configuration for various pages
    
    - Introduced Cypress configuration file for end-to-end testing.
    - Added Cypress tasks for fetching entry frontmatter and random slugs.
    - Created multiple test files for various pages including Blog, Home, Community Hub, and more, ensuring metadata and link validation.
    - Updated package.json to include Cypress scripts for running tests.
    - Enhanced TypeScript configuration to support Cypress paths.
    
    This commit establishes a robust testing framework for the application, improving test coverage and reliability.
    
    * Upgrade Cypress to version 15.0.0
    
    * Add Suspense to SiteLayout to resolve hydration error
    
    - Wrapped StructuredDataScript in a Suspense component to improve loading performance and handle potential delays in data fetching.
    - Upgraded Cypress dependency from version 14.5.4 to 15.0.0 to leverage new features and improvements in end-to-end testing.
    
    * CR - Use underscore for consistency with file naming
    
    * CR - Refactor
    
    * Remove unused excludeMetadata property from multiple Cypress test files to streamline metadata handling. Update createMetadata function to utilize fallback image from graphicsData, enhancing image management across the application.
    
    * chore: Remove redundant commented import
    
    * Refactor Cypress task and util functions
    
    - Consolidated task imports in cypress.config.ts to improve clarity and maintainability.
    - Updated multiple test files to utilize new utility functions for generating meta titles and frontmatter, enhancing consistency across tests.
    - Removed deprecated utility functions and adjusted imports to reflect the new structure, ensuring better organization of code.
    - Introduced new types for frontmatter to improve type safety and clarity in the codebase.
    
    * Move Cypress test utilities to shared package
    
    * Move Cypress task, utils and support to shared folder
    
    * - Removed the `markdownExtension.ts` file and updated imports in multiple task files to use the new shared constant for markdown file extensions.
    - This change enhances maintainability by centralizing the definition of file extensions and reducing redundancy in the codebase.
    - Added a new support entry point in the package.json for better organization of Cypress support files.
    
    * Remove unused markdown extension
    
    * Refactor meta title generation in Cypress tests
    
    - Updated multiple Cypress test files to utilize new utility functions for generating meta titles, enhancing consistency and maintainability across tests.
    - Removed redundant imports and parameters related to root metadata, simplifying the meta title generation process.
    - Introduced new utility files for `getMetaTitleTemplate` and `getMetaTitleWithSuffix` to centralize logic and improve code organization.
    
    This refactor streamlines the handling of meta titles in tests, ensuring a more efficient and cleaner codebase.
    
    ---------
    
    Co-authored-by: Mirha Masala <mirha@hey.com>
    Co-authored-by: Charly Martin <charly@hey.com>

---
+++ packages/cypress/src/tasks/getPageFrontmatterSeo.ts
@@ -0,0 +1,14 @@
+import fs from 'fs/promises'
+
+import matter from 'gray-matter'
+
+import type { PageFrontmatterSeo } from '@filecoin-foundation/utils/types/genericEntryFrontmatterType'
+
+import { MARKDOWN_EXTENSION } from '@filecoin-foundation/utils/constants/fileExtensions'
+
+export async function getPageFrontmatterSeo(contentPath: string) {
+  const file = await fs.readFile(contentPath + MARKDOWN_EXTENSION, 'utf8')
+  const { data } = matter(file)
+
+  return data.seo as PageFrontmatterSeo
+}
